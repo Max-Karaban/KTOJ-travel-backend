22e59aa1bfb2f20bc34cdce1d4983b771b1fc265
Max-Karaban <maxkill406@gmail.com>
2025-05-04 20:58:26 +0200

    place.js

--- models/place.js
+++ models/place.js
@@ -1,15 +1,14 @@
-// models/place.js
 const crypto = require("crypto");
 
 module.exports = {
   createPlaceObject: (dtoIn) => {
     return {
-      id: crypto.randomUUID(),           // unique place ID
-      tripId: dtoIn.tripId,              // ID of related trip
-      name: dtoIn.name,                  // place name
-      location: dtoIn.location,          // location
-      description: dtoIn.description,    // description
-      note: dtoIn.note || ""             // optional user note
+      id: crypto.randomUUID(),
+      tripId: dtoIn.tripId,
+      name: dtoIn.name,
+      location: dtoIn.location,
+      description: dtoIn.description,
+      note: dtoIn.note || ""
     };
   }
 };
